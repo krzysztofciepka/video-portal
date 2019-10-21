3a9a8851e001748fe9e253845704ad4ab2866d41
Krzysztof Ciepka <krzysztof.ciepka@blue-focus.com>
2019-10-21 12:14:05 +0200

    Misc fixes

--- views/includes/site.js
+++ views/includes/site.js
@@ -1,14 +1,19 @@
 window.addEventListener('load', () => {
   // handle search field
-  document.getElementById('search').addEventListener('keyup', (e) => {
-    e.preventDefault(); 
-    if ((e.keyCode == 13))  {
-      window.location.href='/videos?search=' + e.target.value
-    }
-  })
+  if(document.getElementsById('search')){
+    document.getElementById('search').addEventListener('keyup', (e) => {
+      e.preventDefault(); 
+      if ((e.keyCode == 13))  {
+        window.location.href='/videos?search=' + e.target.value
+      }
+    })
+  }
+
   //handle sorting selection
-  document.getElementById('sort').addEventListener('change', (e) => {
-    e.preventDefault(); 
-    window.location.href='?sort=' + e.target.value
-  })
+  if(document.getElementsById('sort')){
+    document.getElementById('sort').addEventListener('change', (e) => {
+      e.preventDefault(); 
+      window.location.href='?sort=' + e.target.value
+    })
+  }
 })
